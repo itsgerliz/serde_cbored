1c17b3fda227f469b0bd37700019d04c85e2e3b4
itsgerliz <itsgerliz@bonai.es>
2025-12-29 01:55:10 +0100

    chore: remove unused major types

--- src/lib.rs
+++ src/lib.rs
@@ -11,5 +11,3 @@ pub(crate) const BYTE_STRING: u8        = 0b010_00000;
 pub(crate) const TEXT_STRING: u8        = 0b011_00000;
 pub(crate) const ARRAY_OF_ITEMS: u8     = 0b100_00000;
 pub(crate) const MAP_OF_ITEMS: u8       = 0b101_00000;
-pub(crate) const TAGGED_ITEM: u8        = 0b110_00000;
-pub(crate) const FLOAT_SIMPLE_BREAK: u8 = 0b111_00000;
